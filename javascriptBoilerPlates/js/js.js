b17b9b187d6840dc3d72b622afe6737c34a0cdfd
Ana <eaaania@students.eaaa.dk>
2020-02-05 08:46:28 +0100

    something

--- javascriptBoilerPlates/js/js.js
+++ javascriptBoilerPlates/js/js.js
@@ -3,3 +3,20 @@
  * purpose: interactivity
  **/
 console.log('Stardate 235678.89: JavaScript from js/js.js is up and running.');
+
+// the image names should be real image names
+var myGallery = [
+  "11.jpg",
+  "22.jpg",
+  "33.jpg",
+  "44.jpg",
+  "55.jpg",
+  "66.jpg"
+];
+
+for (var i = 0; i < myGallery.length; i++) {
+	// note the shorthand code below
+    exhibition.innerHTML += '<img src="images/' + myGallery[i] + '" alt="from my gallery">';
+}
+// let the loop create the HTML for your gallery
+
